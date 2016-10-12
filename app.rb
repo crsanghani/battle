ca0f823269bb77dbccb267fcf287e672155a8838
James <James.Steel@me.com>
2016-10-12 11:03:38 +0100

    refactored

--- app.rb
+++ app.rb
@@ -12,8 +12,8 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :play
   end
 
@@ -25,9 +25,9 @@ class Battle < Sinatra::Base
   end
 
   get '/attack' do
-    @player_1_name = $player_1
-    @player_2_name = $player_2
-    Game.new.attack(@player_2_name)
+    @player_1 = $player_1
+    @player_2 = $player_2
+    Game.new.attack(@player_2)
     erb :attack
   end
   # start the server if ruby file executed directly
