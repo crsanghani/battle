8508e423db349a04c7ee3e13fa4c9053a0dae4af
James <James.Steel@me.com>
2016-10-12 10:29:56 +0100

    working tests, commiting before making a few changes

--- app.rb
+++ app.rb
@@ -25,8 +25,9 @@ class Battle < Sinatra::Base
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1_name = $player_1
+    @player_2_name = $player_2
+    Game.new.attack(@player_2_name)
     erb :attack
   end
   # start the server if ruby file executed directly
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -7,8 +7,8 @@ feature 'Attacking' do
 
   scenario 'reduce hit points by 10' do
     sign_in_and_play
-    click_button 'Attack'
-    click_link 'OK'
+    click_link 'Attack'
+    #click_link 'OK'
     expect(page).not_to have_content 'Mittens: 60HP'
     expect(page).to have_content 'Mittens: 50HP'
   end
